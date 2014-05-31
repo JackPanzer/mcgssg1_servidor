e5d2326b0e9bd6072f06d0c881a98098c590599c
Victor <Victor@VAF>
2014-05-31 20:43:44 +0200

    creada funcion getAllDestinos

--- mgcssg1_servidor/services.php
+++ mgcssg1_servidor/services.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * Obtenemos todos los destinos para un Usuario alumno
+ * conocido su Id
+ * Serán validos aquellos destinos de los que tengamos registradas
+ * Asignaturas Extrangeras que convalides con alguna de la titulación del alumno*/
+function getAllDestinos($idAlumno){
+	
+//TODO
+	
+}
+
+
+
+?>
