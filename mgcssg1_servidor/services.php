bc31cb13e17a3a8a9b8039faebf2578b01385ad4
Juan Manuel Pedraza García <jackpanzer2012@gmail.com>
2014-06-26 20:17:33 +0200

    Consulta editarDestinos modificada.

--- mgcssg1_servidor/services.php
+++ mgcssg1_servidor/services.php
@@ -180,7 +180,7 @@ function editarDestino($idDestino, $nombre, $idPais, $idIdioma, $disponible, $nu
 	if ($conexion) {
 		$esquema = mysql_select_db ( DB_NAME, $conexion );
 		if ($esquema) {
-			$query = sprintf ( "UPDATE FROM Destino SET nombre='%s' 
+			$query = sprintf ( "UPDATE Destino SET nombre='%s' 
 					pais=%d idioma=%d disponible=%d numPlazas=%d nvlRequerido=%d 
 					WHERE id=%d;", mysql_escape_string ( $nombre ), $idPais, $idIdioma, $disponible, $numPlazas, $nvlRequerido, $idDestino );
 			logToFile("editarDestino.txt", $query);
